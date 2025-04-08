1526fdf7f1035336673d6f06085acd8eb6eee2ad
Andrew Wong <andrewwong@dhcp-10-142-211-149.uawifi.arizona.edu>
2025-04-08 10:43:33 -0700

    update gameboard class

--- Scrabble/src/Model/GameBoard.java
+++ Scrabble/src/Model/GameBoard.java
@@ -1,9 +1,24 @@
 package Model;
 
 public class GameBoard {
-	private Tile board[][];
+	private Tile[][] board;
 	
 	public GameBoard() {
-		
+		board = new Tile[15][15];
+	}
+	
+	public Tile getTile(int i, int j) {
+		return board[i][j];
+	}
+	
+	public void placeGamePiece(int i, int j) {
+		Tile tile = getTile(i,j);
+		if (tile.isEmpty()) {
+			// TODO implement helper functions that checks if placement i, j is valid
+		}
+	}
+
+	public boolean checkValidMove() {
+		// TODO check if move is valid
 	}
 }
