84cd0a31eb447de642b30d6000ce616478aa1026
Erik Picazzo <epicazzo@arizona.edu>
2025-04-10 23:24:32 -0700

    Added Code to set up the special tiles on the GameBoard

--- Scrabble/src/Model/GamePiece.java
+++ Scrabble/src/Model/GamePiece.java
@@ -115,6 +115,7 @@ public class GamePiece {
 		}
 	}
 	
+	
 	@Override
 	public String toString() {
 		return "" + this.letter + ": with a point value of: " + this.value;
--- Scrabble/src/Model/Tile.java
+++ Scrabble/src/Model/Tile.java
@@ -3,6 +3,10 @@ package Model;
 public class Tile {
 
 	private GamePiece piece;
+	public enum Type {Blank, Center, TripleWord, TripleLetter, DoubleWord, DoubleLetter};
+	private Type type;
+	private boolean used;
+	
 	
 	public Tile() {
 		piece = null;
@@ -14,6 +18,25 @@ public class Tile {
 	
 	public void setPiece(GamePiece piece) {
 		this.piece = piece;
+	}
+	
+	public void setSpecial(Type type) {
+		this.type = type;
+	}
+	
+	@Override
+	public String toString() {
+		if (this.piece != null) {
+			return "A"; // Has to be the piece's letter but I don't currently know how to get that
+		}
+		else {
+			if (type == Type.Center) return "**";
+			else if (type == Type.TripleWord) return "TW";
+			else if (type == Type.TripleLetter) return "TL";
+			else if (type == Type.DoubleWord) return "DW";
+			else if (type == Type.DoubleLetter) return "DL";
+			else return "  ";
+		}
 	}
 	
 }
