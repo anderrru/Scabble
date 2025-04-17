4574b56edcd6d38364b2dfa762876324ee06b9aa
andrewhuynh10 <166893576+andrewhuynh10@users.noreply.github.com>
2025-04-17 09:50:06 -0700

    Add files via upload

--- Scrabble/src/Model/Player.java
+++ Scrabble/src/Model/Player.java
@@ -2,4 +2,26 @@ package Model;
 
 public class Player {
 
+    private final String name;
+    private final Hand hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new Hand(); 
+    }
+
+    // Returns the player's name
+    public String getName() {
+        return name;
+    }
+
+    // Returns the full Hand object 
+    public Hand getHand() {
+        return hand;
+    }
+
+    // Returns a string display of the player's hand 
+    public String getPlayerHand() {
+        return hand.toString();
+    }
 }
