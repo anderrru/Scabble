5b6d2b2a28c404f3d0fca678b0baeb85f4c06b5f
Johnathan-Alexander <johnathana040@arizona.edu>
2025-04-18 15:33:04 -0700

    Update Move.java
    Compiling all code to main branch

--- Scrabble/src/Model/Move.java
+++ Scrabble/src/Model/Move.java
@@ -1,56 +1,86 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 
-public class Move implements Iterable<GamePiece>{
-	private ArrayList<GamePiece> move;
+public class Move {
+	private HashMap<Position, GamePiece> move;
 	public enum Directions {Vertical, Horizontal};
 	private Directions direction;
 	private int startX;
 	private int startY;
 	private WordMap wordMap;
-	
+
 	public Move() {
-		this.move = new ArrayList<GamePiece>();
+		this.move = new HashMap<>();
 		this.wordMap = new WordMap();
 	}
-	
-	public void addPiece(GamePiece piece) {
+
+	public void addPiece(GamePiece piece, int x, int y) {
 		/*
 		 * This method adds a game piece
 		 */
-		move.add(piece);
+		Position pos = new Position(x, y);
+		move.put(pos, piece);
 	}
-	
+
 	public void clear() {
 		move.clear();
 	}
-	
-	public boolean checkValidity() {
-		String currentWord = "";
-		for (GamePiece letter: this) {
-			currentWord += letter;
-		}
-		return wordMap.getWord(currentWord.toUpperCase().hashCode()) != null;
-	}
-	
+
 	public int getX() {
 		return this.startX;
 	}
-	
+
 	public int getY() {
 		return this.startY;
 	}
-	
+
 	public void setPlacement(int x, int y, Directions direction) {
 		this.startX = x;
 		this.startY = y;
 		this.direction = direction;
 	}
-
-	@Override
-	public Iterator<GamePiece> iterator() {
-		return move.iterator();
+	
+	public HashMap<Position, GamePiece> getMove() {
+		return this.move;
 	}
+
+	public GamePiece getPiece(Position pos) {
+		return this.move.get(pos);
+	}
+
+	public ArrayList<Position> getPositionsbyX(){
+		ArrayList<Position> positions = new ArrayList<Position>(move.keySet());
+		Collections.sort(positions, Position.sortByXComparator());
+		return positions;
+	}
+	
+	public ArrayList<Position> getPositionsbyY(){
+		ArrayList<Position> positions = new ArrayList<Position>(move.keySet());
+		Collections.sort(positions, Position.sortByYComparator());
+		return positions;
+	}
+	
+	public Directions getDirection() {
+		ArrayList<Position> sortByX = this.getPositionsbyX();
+		ArrayList<Position> sortByY = this.getPositionsbyY();
+		Directions direction = null;
+		int i = 1;
+		while (i < move.size()) {
+			if (sortByX.get(i).getX() == sortByX.get(i-1).getX()) direction = Directions.Vertical;
+			if (sortByY.get(i).getY() == sortByY.get(i-1).getY()) direction = Directions.Horizontal;
+			i++;
+		}
+		return direction;
+	}
+
+	public Position getStartPosition() {
+		if (Directions.Horizontal == this.getDirection())
+			return this.getPositionsbyY().getFirst();
+		else return this.getPositionsbyX().getFirst();
+	}
+
 }
