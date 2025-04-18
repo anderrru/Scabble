81d425fd69773c30339ef77ed13583f7b8d07dc4
Johnathan-Alexander <johnathana040@arizona.edu>
2025-04-18 15:34:22 -0700

    Create Position.java
    Compiling all code to main branch

---
+++ Scrabble/src/Model/Position.java
@@ -0,0 +1,44 @@
+package Model;
+
+import java.util.Comparator;
+
+public class Position{
+	private Integer x;
+	private Integer y;
+	public enum Directions {Horizontal, Vertical};
+	private Directions direction;
+	
+	public Position (int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+
+	public static Comparator<Position> sortByXComparator() {
+		return new Comparator<Position>() {
+			public int compare(Position p1, Position p2) {
+				int comp = p1.x.compareTo(p2.x);
+				if(comp == 0)
+					comp = p1.y.compareTo(p2.y);
+				return comp;
+			}
+		};
+	}
+	
+	public static Comparator<Position> sortByYComparator() {
+		return new Comparator<Position>() {
+			public int compare(Position p1, Position p2) {
+				int comp = p1.y.compareTo(p2.y);
+				if(comp == 0)
+					comp = p1.x.compareTo(p2.x);
+				return comp;
+			}
+		};
+	}
+}
