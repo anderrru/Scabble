222a0fb6d0f4121c22a4c8af37602e01d03c082a
andrewhuynh10 <166893576+andrewhuynh10@users.noreply.github.com>
2025-04-18 15:40:20 -0700

    Add files via upload

---
+++ Scrabble/src/TestModel/GamePieceTests.java
@@ -0,0 +1,46 @@
+package tests;
+
+import Model.GamePiece;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GamePieceTests {
+
+    @Test
+    public void testRemainingTilesInitiallyCorrect() {
+        int remaining = GamePiece.remainingTiles();
+        assertTrue(remaining > 0);
+        // Given we will remove 1 GamePiece then new pool will 
+        // contain 97 pieces instead of 98
+        assertEquals(97, remaining); 
+    }
+
+    @Test
+    public void testGetPieceReducesBagSize() {
+        int before = GamePiece.remainingTiles();
+        GamePiece piece = GamePiece.getPiece();
+        int after = GamePiece.remainingTiles();
+
+        assertNotNull(piece);
+        assertEquals(before - 1, after);
+    }
+
+    @Test
+    public void testGetLetterIsUppercase() {
+        GamePiece piece = GamePiece.getPiece();
+        assertNotNull(piece);
+
+        String letter = piece.getLetter();
+        assertTrue(letter.matches("[A-Z]"));
+    }
+
+
+   @Test
+    public void testGetPieceReturnsNullIfEmpty() {
+        while (GamePiece.getPiece() != null) { }
+
+        assertEquals(0, GamePiece.remainingTiles());
+        assertNull(GamePiece.getPiece());
+    }
+}
