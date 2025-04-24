10eb55b48ad5e45479b71ab559820fe2d678538c
Johnathan-Alexander <johnathana040@arizona.edu>
2025-04-24 10:37:15 -0700

    Commented Letter.java

--- Scrabble/src/Model/Letter.java
+++ Scrabble/src/Model/Letter.java
@@ -1,3 +1,9 @@
+/*
+* Authors: Johnathan Alexander, Erik Picazzo, Andrew Wong, Andrew Huynh
+*
+* Description: This enum class contains values for letters associated with GamePiece objects.
+*/
+
 package Model;
 
 public enum Letter { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z }
