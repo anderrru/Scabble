203b58dff1a4ff6b1ca1b2b4386432e843c27095
Johnathan-Alexander <johnathana040@arizona.edu>
2025-04-25 08:26:42 -0700

    Finished commenting Move.java

--- Scrabble/src/Model/Move.java
+++ Scrabble/src/Model/Move.java
@@ -30,55 +30,79 @@ public class Move {
 
 	public void addPiece(GamePiece piece, int x, int y) {
 		/*
-		 * This method adds a game piece
+		 * This method adds a game piece to the move HashMap at a given x/y position,
+   		 * in the form of a Position object. 
 		 */
 		Position pos = new Position(x, y);
 		move.put(pos, piece);
 	}
 
 	public void clear() {
+		// This method clears the move HashMap
 		move.clear();
 	}
 
 	public int getX() {
+		// This method returns the beginning x-coordinate of a Move
 		return this.startX;
 	}
 
 	public int getY() {
+		// This method returns the beginning y-coordinate of a Move
 		return this.startY;
 	}
 
 	public void setPlacement(int x, int y, Directions direction) {
+		// This method initializes the beginning x, y, and direction of a Move object
 		this.startX = x;
 		this.startY = y;
 		this.direction = direction;
 	}
 
+	// Do we really need this? Move shouldn't be something that can be changed after the initial stuff, yeah?
 	public void setDirection(Directions direction) {
+		// This method sets the direction of a Move object
 		this.direction = direction;
 	}
 	
 	public HashMap<Position, GamePiece> getMove() {
+		// This method returns the move HashMap
 		return this.move;
 	}
 
 	public GamePiece getPiece(Position pos) {
+		// This method returns the GamePiece associated with a given x/y Position object
 		return this.move.get(pos);
 	}
 
 	public ArrayList<Position> getPositionsbyX(){
+		// This method utilizes Collections, in conjunction with the Comparator interface, 
+		// to sort all of the Position objects in the move HashMap by their x-coordinate value,
+		// in increasing numerical order
 		ArrayList<Position> positions = new ArrayList<Position>(move.keySet());
 		Collections.sort(positions, Position.sortByXComparator());
 		return positions;
 	}
 	
 	public ArrayList<Position> getPositionsbyY(){
+		// This method utilizes Collections, in conjunction with the Comparator interface,
+		// to sort all of the Position objects in the move HashMap by their y-coordinate value,
+		// in increasing numerical order. 
 		ArrayList<Position> positions = new ArrayList<Position>(move.keySet());
 		Collections.sort(positions, Position.sortByYComparator());
 		return positions;
 	}
 
 	public Directions getDirection() {
+		/*
+		* This method returns the direction of the move.
+  		*
+    		* In the event that the direction variable has not yet been set,
+      		* the method calculates the direction by ordering the x and y coordinates
+		* via the Comparator interface, then checks to see which axis value
+  		* undergoes changes. If the y-coordinate changes as the Positions are
+    		* sorted, then the direction of the Move is vertical. Else, it is horizontal.
+		*/
 		if (this.direction != null) {
 			return this.direction;
 		}
@@ -86,18 +110,28 @@ public class Move {
 		ArrayList<Position> sortByY = this.getPositionsbyY();
 		Directions direction = null;
 		int i = 1;
+
+		// Checks to see which axis is changing as the Position objects are iterated. 
 		while (i < move.size()) {
 			if (sortByY.get(i).getX() == sortByY.get(i-1).getX()) direction = Directions.Vertical;
 			else direction = Directions.Horizontal;
 			i++;
 		}
+		
 		return direction;
 	}
 
 	public Position getStartPosition() {
+		// This method returns the start coordinate of the move HashMap
 		Move.Directions direction = this.getDirection();
+
+		// First checks to see if a direction has been set, or if the Move HashMap 
+		// has attempted placements of GamePieces
 		if (direction == null || move.isEmpty()) return null;
 
+		// If a direction has been set, it checks the directionality to ensure it returns the proper value
+		// by ordering the Position objects in the HashMap by their x-coordinate for Horizontal, and 
+		// y-coordinate for Vertical. 
 		if (direction == Directions.Horizontal) {
 			return this.getPositionsbyY().get(0);
 		} else {
@@ -107,6 +141,7 @@ public class Move {
 
 
 	public GamePiece getAtPosition(int x, int y) {
+		// This method returns the GamePiece at its associated x/y Position object. 
 		for (Position p : move.keySet()) {
 			if (p.getY() == y && p.getX() == x) {
 				return move.get(p);
