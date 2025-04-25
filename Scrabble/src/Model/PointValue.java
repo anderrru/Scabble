07812c234c84d690d694cf3aabaf73932b91ac36
Johnathan-Alexander <johnathana040@arizona.edu>
2025-04-25 08:45:27 -0700

    Commented PointValue.java

--- Scrabble/src/Model/PointValue.java
+++ Scrabble/src/Model/PointValue.java
@@ -1,3 +1,10 @@
+/*
+* Authors: Johnathan Alexander, Erik Picazzo, Andrew Wong, Andrew Huynh
+*
+* Description: This file contains the enums for the point values associated with
+*		various Scrabble GamePiece objects. 
+*/
+
 package Model;
 
 public enum PointValue { 
